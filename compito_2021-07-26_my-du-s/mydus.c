f7788e9915da4862be749ed242db28dc4c0b27a4
aremi0 <gabriele.passarello98@gmail.com>
2022-07-19 20:37:38 +0200

    upload iniziale mydus

---
+++ compito_2021-07-26_my-du-s/mydus.c
@@ -0,0 +1,20 @@
+/**
+ * @file mydus.c
+ * @author aremi
+ * @version 0.1
+ * @date 2022-07-19
+ * 
+ * Prova di laboratorio di SO del 2021-07-26
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <wait.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/stat.h>
+#include <dirent.h>
