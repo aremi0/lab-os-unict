5cc54751959e32a144ef567753dd067d3deb035e
aremi0 <gabriele.passarello98@gmail.com>
2022-07-20 22:20:28 +0200

    up: shm from vector to struct & better recursion

--- compito_2021-07-26_my-du-s/mydus.c
+++ compito_2021-07-26_my-du-s/mydus.c
@@ -20,24 +20,22 @@
 #include <sys/stat.h>
 #include <dirent.h>
 
-#define MAX_PATH_LEN 2048
+#define MAX_PATH_LEN 4096
 
 #define S_STATER 0      //sync
 #define MUTEX 1         //mutex-sync
 
-/***
- *      ||SHM_PATH :    (char *)
- *  | p+0 ==> usato da scanner[i] ==> path-di-ogni-file ==> race conditions => MUTEX
- * 
- *      ||SHM_COUNTER :    (int *)
- *  | p+0 ==> usato da scanner[i] ==> numero di scanner[i] da decrementare ==> race conditions => MUTEX
-*/
+typedef struct{
+    unsigned eof;
+    unsigned id;
+    char pathFile[MAX_PATH_LEN];
+}shm_msg;
 
 typedef struct{
     long type;
-    int eof;
-    char pathFile[MAX_PATH_LEN];
-    long nBlocks;
+    unsigned eof;
+    unsigned id;
+    unsigned long nBlocks;
 } msg;
 
 int WAIT(int sem_des, int semNum){
@@ -50,148 +48,118 @@ int SIGNAL(int sem_des, int semNum){
     return semop(sem_des, op, 1);
 }
 
-void recursiveScan(char* currentPath, char* rootPath, int sem, char *p){
+void scanner(char id, int shm, int sem, char *path, char casoBase){
     DIR *dir;
     struct dirent *entry;
-    struct stat statbuf;
-    char previousDir[MAX_PATH_LEN];
+    shm_msg *p;
 
-    if((dir = opendir(rootPath)) == NULL){ //apro la directory del path
+    if((p = (shm_msg*)shmat(shm, NULL, 0)) == (shm_msg*)-1){ //attach al segmento condiviso path
+        perror("shmat scanner");
+        exit(1);
+    }
+    if((dir = opendir(path)) == NULL){ //apro la directory del path
         perror("opendir scanner recursion");
         exit(1);
     }
-    if((chdir(rootPath)) == -1){ //cambio la current-working-directory alla directory da scansionare...
-        perror("chdir scanner recursion");
-        exit(1);
-    }
 
-    strcat(currentPath, "/"); //predispongo currentPath alla concatenazione...
-    
     while((entry = readdir(dir))){ //mentre punto un file della directory...
-        lstat(entry->d_name, &statbuf); //raccolgo info sul file puntato
-
         if((strcmp(entry->d_name, ".") == 0) || (strcmp(entry->d_name, "..") == 0)) //se punto la dir "." o ".." vado avanti
             continue;
-        else if(S_ISDIR(statbuf.st_mode)){ //se è una qualsiasi altra dir avvio ricorsione su di essa...
-            strcpy(previousDir, currentPath); //salvo la dir attuale per quando ritorno dalla ricorsione
-            strcat(currentPath, entry->d_name);
-            recursiveScan(currentPath, entry->d_name, sem, p);
-            strcpy(currentPath, previousDir); //sono uscito dalla sotto-directory, ripristino path
+        else if(entry->d_type == DT_DIR){ //se è una qualsiasi altra dir avvio ricorsione su di essa...
+            char tmp[MAX_PATH_LEN];
+            sprintf(tmp, "%s/%s", path, entry->d_name);
+            scanner(id, shm, sem, tmp, 0);
         }
-        else if(S_ISREG(statbuf.st_mode)){ //se è un file regolare...
+        else if(entry->d_type == DT_REG){ //se è un file regolare...
             WAIT(sem, MUTEX); //chiedo permesso di scrivere nella shm
-            strcpy(p, currentPath);
-            strcat(p, entry->d_name); //...scrivo il path del file in shm
+            sprintf(p->pathFile, "%s/%s", path, entry->d_name);
+            p->eof = 0;
+            p->id = id; //...scrivo il path del file in shm
             SIGNAL(sem, S_STATER); //segnalo a stater che è presente un path da elaborare... rilascerà lui la shm
         }
     }
 
-    chdir("..");
     closedir(dir);
-    return;
+
+    if(casoBase){
+        WAIT(sem, MUTEX);
+        p->eof = 1;
+        SIGNAL(sem, S_STATER);
+        shmdt(p);
+        printf("\t\t[SCANNER[%d]] terminazione...\n", id);
+        exit(0);
+    }
 }
 
-void scanner(int shm_p, int shm_c, int sem, char *rootPath){
-    char *p_path, currentPath[MAX_PATH_LEN];
-    int *p_count;
-
-    if((p_path = (char*)shmat(shm_p, NULL, 0)) == (char*)-1){ //attach al segmento condiviso path
-        perror("shmat scanner");
-        exit(1);
-    }
-    if((p_count = (int*)shmat(shm_c, NULL, 0)) == (int*)-1){ //attach al segmento condiviso count
-        perror("shmat stater");
-        exit(1);
-    }
-
-    strcat(currentPath, rootPath);
-    recursiveScan(currentPath, rootPath, sem, p_path);
-    
-    WAIT(sem, MUTEX);
-    *p_count -= 1;
-    if(*p_count == 0)
-        SIGNAL(sem, S_STATER); //segnalo eof a
-    SIGNAL(sem, MUTEX);
-
-    shmdt(p_count);
-    shmdt(p_path);
-    printf("\t\t[SCANNER[i]] terminazione...\n");
-    exit(0);
-}
-
-void stater(int shm_p, int shm_c, int sem, int coda, int numScanner){
+void stater(int shm, int sem, int coda, int numScanner){
     struct stat statbuf;
-    char *p_path;
-    int *p_count;
+    shm_msg *p;
     msg messaggio;
-
-    if((p_path = (char*)shmat(shm_p, NULL, 0)) == (char*)-1){ //attach al segmento condiviso path
-        perror("shmat stater");
-        exit(1);
-    }
-    if((p_count = (int*)shmat(shm_c, NULL, 0)) == (int*)-1){ //attach al segmento condiviso count
-        perror("shmat stater");
-        exit(1);
-    }
+    unsigned eofCounter = 0;
 
     messaggio.eof = 0;
     messaggio.type = 1;
-    WAIT(sem, MUTEX); //setto eof = maxScanner ==> scanner[i] lo decrementano prima di exit(0)
-    *p_count = numScanner;
-    SIGNAL(sem, MUTEX);
 
-    // for(int i = 0; i < numScanner; i++)
-    //     SIGNAL(sem, MUTEX); //dopo aver settato eof sveglio i scanner[i]
+    if((p = (shm_msg*)shmat(shm, NULL, 0)) == (shm_msg*)-1){ //attach al segmento condiviso path
+        perror("shmat stater");
+        exit(1);
+    }
 
     while(1){
         WAIT(sem, S_STATER); //aspetto che uno scanner mi confermi la presenza di un path da elaborare in shm...
 
-        if(*p_count == 0)
-            break;
+        if(p->eof){ //se è true mi hanno svegliato per segnalarmi una exit() e non un nuovo path
+            eofCounter++;
 
-        lstat(p_path, &statbuf); //ottengo info sul file...
-        strcpy(messaggio.pathFile, p_path);
+            if(eofCounter == numScanner)
+                break;
+            else{
+                SIGNAL(sem, MUTEX); //rilascio shm e continuo al prossimo ciclo di attesa...
+                continue;
+            }
+        }
+    
+
+        lstat(p->pathFile, &statbuf); //ottengo info sul file...
+        messaggio.id = p->id;
         messaggio.nBlocks = statbuf.st_blocks;
+        SIGNAL(sem, MUTEX); //posso rilasciare shm
 
         if((msgsnd(coda, &messaggio, sizeof(msg)-sizeof(long), 0)) == -1){ //mando messaggio al padre...
             perror("msgsnd stater");
             exit(1);
         }
-
-        SIGNAL(sem, MUTEX);
     }
 
-
     messaggio.eof = 1;
+
     if((msgsnd(coda, &messaggio, sizeof(msg)-sizeof(long), 0)) == -1){ //mando eof al padre...
         perror("msgsnd stater");
         exit(1);
     }
 
     //in chiusura...
-    shmdt(p_path);
-    shmdt(p_count);
+    shmdt(p);
     printf("\t\t[STATER] terminazione...\n");
     exit(0);
 }
 
 int main(int argc, char *argv[]){
-    int shm_path_d, shm_count_d, sem_d, coda_d;
+    int shm_d, sem_d, coda_d;
     msg messaggio;
-    long scannerBlocks[argc-1];
+    unsigned long scannerBlocks[argc-1];
 
-    memset(&scannerBlocks, 0, sizeof(scannerBlocks)); //azzero vettore dei risultati finali scannerBlocks
+    for(int i = 0; i < argc-1; i++)
+        scannerBlocks[i] = 0;
+
+    //memset(&scannerBlocks, 0, sizeof(scannerBlocks)); //azzero vettore dei risultati finali scannerBlocks
 
     if(argc < 2){
         printf("Uso: %s <path-1> <path-2> ...\n", argv[0]);
         exit(1);
     }
 
-    if((shm_path_d = shmget(IPC_PRIVATE, sizeof(char) * MAX_PATH_LEN+1, IPC_CREAT | IPC_EXCL | 0600)) == -1){ //creazione segmento condiviso path
-        perror("shmget path");
-        exit(1);
-    }
-    if((shm_count_d = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | IPC_EXCL | 0600)) == -1){ //creazione segmento condiviso
+    if((shm_d = shmget(IPC_PRIVATE, sizeof(shm_msg), IPC_CREAT | IPC_EXCL | 0600)) == -1){ //creazione segmento condiviso
         perror("shmget w[i] counter");
         exit(1);
     }
@@ -214,12 +182,10 @@ int main(int argc, char *argv[]){
 
     //creazione figli...
     if(fork() == 0)
-        stater(shm_path_d, shm_count_d, sem_d, coda_d, argc-1);
+        stater(shm_d, sem_d, coda_d, argc-1);
     for(int i = 1; i < argc; i++)
-        if(fork() == 0){
-//            WAIT(sem_d, MUTEX); //aspetto che stater setti l'eof=numScanner prima di startare
-            scanner(shm_path_d, shm_count_d, sem_d, argv[i]);
-        }
+        if(fork() == 0)
+            scanner(i-1, shm_d, sem_d, argv[i], 1);
 
 
     while(1){
@@ -231,22 +197,24 @@ int main(int argc, char *argv[]){
         if(messaggio.eof)
             break;
 
-        for(int i = 0; i < argc-1; i++){ //salvo il numero di blocchi di uno specifico writer[i] path in un vettore
-            if(strstr(messaggio.pathFile, argv[i+1]) != NULL){ //bello sto metodo, ma lo sai cosa comporta vero?
-                scannerBlocks[i] += messaggio.nBlocks / 2; //sto diviso due non lo so perchè...
-                break;
-            }
-        }
+        scannerBlocks[messaggio.id] += messaggio.nBlocks;
+
+        // for(int i = 0; i < argc-1; i++){ //salvo il numero di blocchi di uno specifico writer[i] path in un vettore
+        //     if(strstr(messaggio.pathFile, argv[i+1]) != NULL){ //bello sto metodo, ma lo sai cosa comporta vero?
+        //         scannerBlocks[i] += messaggio.nBlocks / 2; //sto diviso due non lo so perchè...
+        //         break;
+        //     }
+        // }
     }
 
     for(int i = 0; i < argc; i++)
         wait(NULL);
 
-    for(int i = 1; i < argc; i++)
-        printf("%ld\t%s\n", scannerBlocks[i-1], argv[i]);
+    for(int i = 0; i < argc-1; i++)
+        printf("%ld\t%s\n", scannerBlocks[i], argv[i+1]);
 
     //in chiusura...
-    shmctl(shm_path_d, IPC_RMID, NULL);
+    shmctl(shm_d, IPC_RMID, NULL);
     semctl(sem_d, 0, IPC_RMID, 0);
 
     printf("\t\t[PADRE] terminazione...\n");
